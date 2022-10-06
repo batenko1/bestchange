ca99df120344a7017ed5c1a983f82bdb6afd7b9e
johny <batenko4@gmail.com>
2022-10-06 21:18:05 +0300

    update seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Artisan;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,8 +16,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
 
+        Artisan::call('db:wipe');
+
         $this->call([
-            ImportTableSeeder::class
+            ImportTableSeeder::class,
             CurrencySeeder::class,
             DirectionSeeder::class
         ]);
